117365e8ec110b013d1cc0dd0656cdcfbfe21bae
Lê Quốc <quoc020721@gmail.com>
2022-03-21 23:48:06 +0700

    commitlan3

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,8 +3,34 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 export default function ColorTextFields() {
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <Box
       component="form"
@@ -22,14 +48,34 @@ export default function ColorTextFields() {
         <h1>Login</h1>
         <TextField
           sx={{ width: "27%" }}
-          label="Outlined secondary"
+          label="Email address"
           color="secondary"
         />
-        <TextField
-          sx={{ width: "27%" }}
-          label="Outlined secondary"
-          color="secondary"
-        />
+        <FormControl sx={{ m: 1, width: "27%" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+
         <Button sx={{ width: "27%" }} variant="contained">
           LOGIN
         </Button>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Shopping
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
